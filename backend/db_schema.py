e1fea357522e36003b07e847e50eacfc58a09618
Sanjali <sj373@cornell.edu>
2020-11-30 18:12:02 -0500

    op

---
+++ backend/db_schema.py
@@ -0,0 +1,11 @@
+import mysql
+
+# mydb = mysql.connector.connect(
+#     host="localhost",
+#     user="sanjali",
+#     password="sanjali13"
+# )
+
+# mycursor = mydb.cursor()
+
+# mycursor.execute("CREATE DATABASE spotify")
