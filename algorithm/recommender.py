a6795d5d8077b87ba800ce40b30950769fc03f38
Aryan Pandhi <aryan.pandhi@gmail.com>
2020-11-30 23:41:23 -0500

    algorithm

---
+++ algorithm/recommender.py
@@ -0,0 +1,26 @@
+# H := |V| x |E| vertex-hyperedge incidence matrix
+# De := diagonal matrix consisting of hyperedge degrees
+# Dv := diagonal matrix consisting of vertex degrees
+# W := |E| x |E| diagonal matrix containing hyperedge weights
+# 
+# Type of Vertices: 
+#   - Users
+#   - Artists
+#   - Tracks 
+#   - Albums
+#   - Playlists
+# 
+# Type of Relations: 
+#   - User-Track (user and track edge) [0,50|U|]
+#   - User-Artist (user and artist edge) [0,50|U| + num_followed]
+#   - Track-Album (album and its tracks edge)
+#   - Album-Artist (artist and his/her albums edge)
+#   - Track-Track (track similarity edge)
+#   - Playlists (playlist)
+
+def all_artists(data):
+  
+
+def a
+
+
